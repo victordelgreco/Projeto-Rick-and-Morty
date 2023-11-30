62d25ea20cab1941ac5ce6a803f3ba0c017009f8
victordelgreco <victordelgrecogomes@gmail.com>
2023-11-30 15:04:03 -0300

    Adicionei o layout dos cards ao projeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/menu/Menu'
+import CardsComponent from './components/cards/Cards';
 // import Banners from './components/banners/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-<>    <Menu/>
-    {/* <Banners/> */}
+<>    
+<Menu/>
+<CardsComponent/>
+{/* <Banners/> */}
+    
   </>
   );
 }
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,10 +1,20 @@
+import CardComponent from '../card/Card';
+import './cards.css'
+
+export default function CardsComponent() {
+    return (
 
 
-export default function Cards() {
-    return(
-        <>
+        <div className='components-grid'>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        <CardComponent/>
+        </div>
         
-        </>
+        
     )
 }
 
