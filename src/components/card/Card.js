94ece11aa9f0186085f276b687ab43f37ee85efb
victordelgreco <victordelgrecogomes@gmail.com>
2023-12-01 15:18:56 -0300

    Adicionei um arquivo simulando o banco de dados para receber o dados em minha aplicação

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,13 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import GetDataCaracters from '../../db/personagens';
 
-function CardComponent() {
+function CardComponent(params, GetDataCaracters) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{GetDataCaracters.name}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
